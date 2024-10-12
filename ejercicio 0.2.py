e0050a6adceff6a1f2e611e577c2d509f2cf9c48
paula <155648636+fflippy@users.noreply.github.com>
2024-10-12 10:25:57 -0300

    Delete ejercicio 0.2.py

--- ejercicio 0.2.py
+++
@@ -1,9 +0,0 @@
-#ENTRADA
-num1 = int(input("Ingrese un número: "))
-num2 = int(input("Ingrese otro número: "))
-
-#PROCESO
-suma = num1 + num2
-
-#SALIDA
-print(str(num1),"+", str(num2),"=", str(suma)) 
