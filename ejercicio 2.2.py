64cf16eb2d33ef116f93fcefbc8dd1523dd3c136
paula <155648636+fflippy@users.noreply.github.com>
2024-10-12 10:28:48 -0300

    Add files via upload

---
+++ ejercicio 2.2.py
@@ -0,0 +1,11 @@
+#ENTRADA
+q1 = float(input("Ingrese valor de la primera carga: "))
+q2 = float(input("Ingrese valor de la segunda carga: "))
+r = float(input("Ingrese la distancia entre las cargas: "))
+
+#PROCESO
+fuerza = (q1 * q2) / (r ** 2)
+fuerza_final = round(fuerza, 10)
+
+#SALIDA
+print("La fuerza es:", fuerza_final)
